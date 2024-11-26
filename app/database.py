74ebb16689f57ceafdffcfad4155857702fd0b82
Cmarih <marycostajc@gmail.com>
2024-11-25 22:04:22 -0300

    Feature: atualização do database e criação do main

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import time
 
@@ -8,6 +8,22 @@ SQLALCHEMY_DATABASE_URL = "sqlite:/// ./banco.db"
 
 #Criando variáveis para a interação com o banco
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = Sessionmaker(autocommit=False, autoflus=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflus=False, bind=engine)
 
 Base = declarative_base
+
+connected = False
+while not connected:
+    try:
+        engine = engine = create_engine(SQLALCHEMY_DATABASE_URL)
+        connected = True
+    except Exception as e:
+        print(f"Erro ao tentar se conectar: {e}")
+        time.sleep(5)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-print("Oi")
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import database, models, crud
+from app.database import get_db
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"message":"Aulas de Python"}
+
+@app.get("/usuarios/")
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
